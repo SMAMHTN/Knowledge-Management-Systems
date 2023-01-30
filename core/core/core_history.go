caa0c5c003b3e238dc159abfbe2214f9591f86c6
SMAM <57549163+SMAMHTN@users.noreply.github.com>
2023-01-30 12:15:06 +0700

    From Phone

--- core/core/core_history.go
+++ core/core/core_history.go
@@ -1,1 +1,15 @@
 package core
+
+import (
+	t "time"
+)
+
+type History struct {
+	HistoryID          int
+	ActivityType        string
+	Time	t.Time //FIX
+	UserID int
+	Name string
+	Changes string
+	IPAddress string
+}
--- core/core/core_setting.go
+++ core/core/core_setting.go
@@ -1,1 +1,8 @@
 package core
+
+type Setting struct {
+	CompanyID          int
+	CompanyName        string
+	CompanyLogo    []byte //FIX
+	CompanyAddress string
+}
