da883e8969d08c30ddf9ccc93946a37d5a1b70cd
Haickal Fattih Pratama <hfplandd@gmail.com>
2023-07-02 14:59:56 +0700

    login page?

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,91 @@
+"use client"
+import Link from "next/link";
+import { useState } from 'react';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    const credentials = btoa(`aldim:${password}&&${username}`);
+
+    try {
+      const response = await fetch('http://code.smam.my.id:6565/login', {
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          Accept: '*/*',
+        },
+      });
+
+      if (response.ok) {
+        setMessage('Login successful!');
+      } else {
+        setMessage('Login failed!');
+      }
+    } catch (error) {
+      setMessage('An error occurred during login.');
+      console.log(error);
+    }
+  };
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-center text-gray-700">Logo</h1>
+        <form className="mt-6">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+
+          </div>
+          <Link
+            href="/forget"
+            className="text-xs text-blue-600 hover:underline"
+          >
+            Forget Password?
+          </Link>
+          <div className="mt-2">
+            <button onClick={() => handleLogin()} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-800 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-gray-600">
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-4 text-sm text-center text-gray-700">
+          Don't have an account?{" "}
+          <Link
+            href="/signup"
+            className="font-medium text-blue-600 hover:underline"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+      <p>{message}</p>
+    </div>
+  );
+}
