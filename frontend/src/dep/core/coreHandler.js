ae74ef9984c27770aa248161a027da27bb13f884
Aldi Mulyawan <aldismartkid@gmail.com>
2023-08-12 11:02:26 +0700

    Fix Login Checker

--- frontend/src/dep/core/coreHandler.js
+++ frontend/src/dep/core/coreHandler.js
@@ -59,10 +59,10 @@ export async function CoreAPI(method, path, data) {
   const cookieStore = cookies();
   let un, pwd;
   try {
-    un = cookieStore.get("username").value;
-    pwd = cookieStore.get("password").value;
+    let un = cookieStore.get("username")?.value;
+    let pwd = cookieStore.get("password")?.value;
 
-    if (!un || !pwd) {
+    if (un == undefined || pwd == undefined) {
       throw new Error("You must log in.");
     }
   } catch (error) {
@@ -96,10 +96,10 @@ export async function CoreAPIGET(path) {
   const cookieStore = cookies();
   let un, pwd;
   try {
-    un = cookieStore.get("username").value;
-    pwd = cookieStore.get("password").value;
+    let un = cookieStore.get("username")?.value;
+    let pwd = cookieStore.get("password")?.value;
 
-    if (!un || !pwd) {
+    if (un == undefined || pwd == undefined) {
       throw new Error("You must log in.");
     }
   } catch (error) {
--- frontend/src/dep/kms/kmsHandler.js
+++ frontend/src/dep/kms/kmsHandler.js
@@ -8,10 +8,10 @@ export async function KmsAPI(method, path, data) {
   const cookieStore = cookies();
   let un, pwd;
   try {
-    un = cookieStore.get("username").value;
-    pwd = cookieStore.get("password").value;
+    let un = cookieStore.get("username")?.value;
+    let pwd = cookieStore.get("password")?.value;
 
-    if (!un || !pwd) {
+    if (un == undefined || pwd == undefined) {
       throw new Error("You must log in.");
     }
   } catch (error) {
@@ -45,10 +45,10 @@ export async function KmsAPIGET(path) {
   const cookieStore = cookies();
   let un, pwd;
   try {
-    un = cookieStore.get("username").value;
-    pwd = cookieStore.get("password").value;
+    let un = cookieStore.get("username")?.value;
+    let pwd = cookieStore.get("password")?.value;
 
-    if (!un || !pwd) {
+    if (un == undefined || pwd == undefined) {
       throw new Error("You must log in.");
     }
   } catch (error) {
