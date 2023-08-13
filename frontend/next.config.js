08482bbe51e5b21d38ce36dfa52f6fa3ae7f61a4
Aldi Mulyawan <aldismartkid@gmail.com>
2023-08-13 22:31:59 +0700

    increase frontend body limit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
-}
+  experimental: {
+    serverActions: true,
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb", // Set desired value here
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
