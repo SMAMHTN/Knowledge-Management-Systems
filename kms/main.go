5206add58588bd3f00767e133b7a5cc3dcd5bdf1
Aldi Mulyawan <aldismartkid@gmail.com>
2023-08-13 17:17:52 +0700

    up

--- kms/main.go
+++ kms/main.go
@@ -6,8 +6,4 @@ import (
 
 func main() {
 	kms.Test_api()
-	// a, b, c := kms.SolrCallQuery([]int{1, 2, 3, 4, 5, 6, 7}, "Article:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-	// fmt.Println(string(a))
-	// fmt.Println(b)
-	// fmt.Println(c)
 }
