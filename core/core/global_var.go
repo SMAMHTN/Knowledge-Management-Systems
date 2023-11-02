7f0b7c6ba36403b2697b941dba5ff8a1a1025c34
Aldi Mulyawan <aldismartkid@gmail.com>
2023-11-02 22:45:22 +0700

    pre-release-test

--- core/core/global_var.go
+++
@@ -1,7 +0,0 @@
-package core
-
-const AppName string = "core"
-const DatabaseName string = "core"
-
-var ConfigurationFile string
-var InstallDatabase string
--- frontend/src/dep/core/getThemeCookiesValue.js
+++
@@ -1,8 +0,0 @@
-import { getCookie } from 'cookies-next';
-
-function getThemeCookiesValue() {
-  const ThemeValue = getCookie('theme');
-  return JSON.parse(ThemeValue);
-}
-
-export default getThemeCookiesValue;
