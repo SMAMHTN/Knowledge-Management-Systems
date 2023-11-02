c40ec40186cebd7e450f02433c811c615b8b7dbd
Aldi Mulyawan <aldismartkid@gmail.com>
2023-11-02 22:29:14 +0700

    fix build

--- build_and_release.bat
+++ build_and_release.bat
@@ -2,8 +2,10 @@
 docker build -t ghcr.io/smam-tech/kms:v1 .\kms
 docker build -t ghcr.io/smam-tech/core:v1 .\core
 docker build -t ghcr.io/smam-tech/frontend:v1 .\frontend
+docker build -t ghcr.io/smam-tech/kms-solr:v1 .\solr
 
 :: Push the Docker images
 docker push ghcr.io/smam-tech/kms:v1
 docker push ghcr.io/smam-tech/core:v1
 docker push ghcr.io/smam-tech/frontend:v1
+docker push ghcr.io/smam-tech/kms-solr:v1
--- build_and_release.sh
+++ build_and_release.sh
@@ -1,7 +1,9 @@
 docker build -t ghcr.io/smam-tech/kms:v1 ./kms
 docker build -t ghcr.io/smam-tech/core:v1 ./core
 docker build -t ghcr.io/smam-tech/frontend:v1 ./frontend
+docker build -t ghcr.io/smam-tech/kms-solr:v1 ./solr
 
 docker push ghcr.io/smam-tech/kms:v1
 docker push ghcr.io/smam-tech/core:v1
 docker push ghcr.io/smam-tech/frontend:v1
+docker push ghcr.io/smam-tech/kms-solr:v1
