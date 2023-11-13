af1e7db2aa9588134ec0bd12265fc022bd5a8c86
Haickal Fattih Pratama <hfplandd@gmail.com>
2023-11-13 21:06:24 +0700

    fix

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar/Navbar';
 import NavbarMobile from '@/components/Navbar/NavbarMobile';
 
 function Nav(themecolor) {
-  console.log('color runntin----------------------------------------------------------------------------------------------------------------------------------', themecolor);
+  // console.log('color runntin----------------------------------------------------------------------------------------------------------------------------------', themecolor);
   return (
     <div>
       <Navbar theme={themecolor} />
